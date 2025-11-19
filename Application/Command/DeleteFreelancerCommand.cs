d27aeea535650d2f63cbd805b6b7d21bdf5d37b1
hj2583 <haojuan610@gmail.com>
2025-11-19 17:19:00 +0800

    Added primary constructure and healthcheck

--- Application/Command/DeleteFreelancerCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace WebAPI1.Application.Commands
-{
-    public class DeleteFreelancerCommand : IRequest<bool>
-    {
-        public int Id { get; set; }
-    }
-}
--- Application/Queries/GetAllFreelancersQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using WebAPI1.Application.DTOs;
-
-namespace WebAPI1.Application.Queries
-{
-    public class GetAllFreelancersQuery : IRequest<IEnumerable<CreateFreelancerResponse>>
-    {
-    }
-}
